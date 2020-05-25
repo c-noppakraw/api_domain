1206bbe0f7f8e0950e77a801f60af4bc818678da
DriverA <c.noppakraw@gmail.com>
2020-05-25 08:53:29 +0700

    edit url

--- routes/domain.js
+++ routes/domain.js
@@ -5,10 +5,10 @@ const controllers = require('../controllers/domain');
 const model = require('../model/domain');
 
 router.get('/', controllers.list_domain);
-router.post('/create', controllers.add_domain);
+router.post('/', controllers.add_domain);
 router.get('/search', controllers.search_domain);
-router.get('/:id/edit', controllers.get_domain);
-router.put('/:id/edit', controllers.post_domain);
+router.get('/:id', controllers.get_domain);
+router.put('/:id', controllers.post_domain);
 router.delete('/:id', controllers.delete_domain);
 
 module.exports = router;
