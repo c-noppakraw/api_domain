0b0b97695365d06d59bd0e36966acb162c2e7ff6
DriverA <c.noppakraw@gmail.com>
2020-05-26 07:02:35 +0700

    edit call variable

--- routes/domain.js
+++ routes/domain.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../config/db');
 const controllers = require('../controllers/domain');
-const model = require('../model/domain');
 
 router.get('/', controllers.list_domain);
 router.post('/', controllers.add_domain);
