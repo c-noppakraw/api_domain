1371732a439d937359799ac1e33bd9b351777313
DriverA <c.noppakraw@gmail.com>
2020-06-01 20:38:04 +0700

    call fn controllers.line

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 // });
+require('../controllers/line');
 router.use('/domains', require('./domain'));
 router.use('/ssl', require('./ssl'));
 router.use('/transaction', require('./payment'));
